cab540a3e75202c7b1cb5d7c8c2319dd5ad0ce68
Marco <marco.canovese@studenti.unipd.it>
2021-10-14 09:29:47 +0200

    first commit

---
+++ challenginator-fe/src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService} from "../_services/auth.service";
+import {TokenStorageService} from "../_services/token-storage.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: any = {
+    email: null,
+    password: null
+  };
+
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  jwt: string | null = '';
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()){
+    this.isLoggedIn = true;
+    this.jwt = this.tokenStorage.getToken();
+  } }
+
+  onSubmit(): void{
+    const {email,password} = this.form;
+
+    this.authService.login(email, password).subscribe(
+      data => {
+
+        this.tokenStorage.saveToken(data.jwt);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.reloadPage();
+      },
+      err => {
+        this.errorMessage = err.error.message();
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage(): void{
+    window.location.reload();
+  }
+
+}
